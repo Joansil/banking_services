30272ebba32846594745fcd7d6eb655410a2524f
Joansil <joansilfs@gmail.com>
2025-01-29 08:53:26 -0300

    update migrations

--- priv/repo/migrations/20250129113851_create_accounts.exs
+++ priv/repo/migrations/20250129113851_create_accounts.exs
@@ -2,12 +2,11 @@ defmodule BankingService.Repo.Migrations.CreateAccounts do
   use Ecto.Migration
 
   def change do
-    create table(:accounts, primary_key: false) do
-      add :id, :binary_id, primary_key: true
+     create table(:accounts, primary_key: false) do
+      add :id, :uuid, primary_key: true, default: fragment("uuid_generate_v4()")
       add :email, :string
       add :name, :string
       add :balance, :decimal
-
       timestamps(type: :utc_datetime)
     end
 
--- priv/repo/migrations/20250129113904_create_transactions.exs
+++ priv/repo/migrations/20250129113904_create_transactions.exs
@@ -3,15 +3,15 @@ defmodule BankingService.Repo.Migrations.CreateTransactions do
 
   def change do
     create table(:transactions, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-      add :amount, :decimal
-      add :status, :string
-      add :account_from_id, references(:accounts, on_delete: :nothing, type: :binary_id)
-      add :account_to_id, references(:accounts, on_delete: :nothing, type: :binary_id)
-
-      timestamps(type: :utc_datetime)
+      add :id, :uuid, primary_key: true, default: fragment("uuid_generate_v4()")
+      add :amount, :decimal, null: false
+      add :status, :string, null: false
+      add :account_from_id, references(:accounts, type: :uuid, on_delete: :nothing), null: false
+      add :account_to_id, references(:accounts, type: :uuid, on_delete: :nothing), null: false
+      timestamps()
     end
 
+    create constraint(:transactions, :amount_positive, check: "amount > 0")
     create index(:transactions, [:account_from_id])
     create index(:transactions, [:account_to_id])
   end
